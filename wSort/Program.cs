5a6f147b4848e99be20c4f27cc97f8a21c7c4ad5
ishellstrike <ishellstrike@gmail.com>
2014-02-21 12:56:57 +0400

    extra

--- wSort/Program.cs
+++ wSort/Program.cs
@@ -11,8 +11,15 @@ namespace wSort
         private static string gifDir = "Animations";
         private static string docDir = "Documents";
         private static string arcDir = "Archives";
+        private static string audDir = "Audio";
+        private static bool extra;
+        private static string jarDir = "Jars";
         static void Main(string[] args)
         {
+            if (args.Contains("-extra")) {
+                extra = true;
+            }
+
             if (!Directory.Exists(picDir)) {
                 Directory.CreateDirectory(picDir);
             }
@@ -28,6 +35,17 @@ namespace wSort
             {
                 Directory.CreateDirectory(arcDir);
             }
+            if (!Directory.Exists(audDir))
+            {
+                Directory.CreateDirectory(audDir);
+            }
+
+            if (extra) {
+                if (!Directory.Exists(jarDir))
+                {
+                    Directory.CreateDirectory(jarDir);
+                }
+            }
 
             foreach (var file in Directory.GetFiles(Directory.GetCurrentDirectory())) {
                 var fi = new FileInfo(file);
@@ -37,21 +55,28 @@ namespace wSort
                     case ".bmp":
                     case ".tga":
                     case ".jpeg":
-                        File.Move(file, picDir + "\\" + fi.Name);
+                        Mover(file, picDir, fi);
                         break;
 
                     case ".gif":
-                        File.Move(file, gifDir + "\\"+ fi.Name);
+                        Mover(file, gifDir, fi);
                         break;
 
                     case ".zip":
-                    case ".gz":
-                    case ".tar":
-                    case ".bz2":
-                    case ".tgz":
                     case ".rar":
                     case ".7z":
-                        File.Move(file, arcDir + "\\" + fi.Name);
+                    case ".tar":
+                    case ".gz":
+                    case ".bz2":
+                    case ".tgz":
+                        Mover(file, arcDir, fi);
+                        break;
+
+                    case ".mp3":
+                    case ".flac":
+                    case ".wav":
+                    case ".ogg":
+                        Mover(file, audDir, fi);
                         break;
 
                     case ".txt ":
@@ -66,9 +91,31 @@ namespace wSort
                     case ".mcdx":
                     case ".xps":
                     case ".dpr":
-                        File.Move(file, docDir + "\\" + fi.Name);
+                        Mover(file, docDir, fi);
                         break;
                 }
+                if (extra) {
+                    switch (fi.Extension) {
+                        case ".jar":
+                        case ".war":
+                            Mover(file, jarDir, fi);
+                            break;
+                    }
+                }
+            }
+        }
+
+        private static void Mover(string file, string dir, FileInfo fi) {
+            try {
+                File.Move(file, dir + "\\" + fi.Name);
+            }
+            catch (Exception) {
+                int i = 1;
+                var noex = (dir+"\\"+fi.Name).Replace(fi.Extension, "");
+                while (File.Exists(noex+"("+i+")"+fi.Extension)) {
+                    i++;
+                }
+                File.Move(file, noex+"("+i+")"+fi.Extension);
             }
         }
     }
